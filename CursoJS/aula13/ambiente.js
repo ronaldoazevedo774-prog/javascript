2e7066db718b393c4e9ad8bb0f4a555cd2126a6f
ronaldoazevedo774-prog <ronaldoazevedo774@gmail.com>
2026-01-20 19:24:58 -0300

    atualizei o exercicio

---
+++ CursoJS/aula13/ambiente.js
@@ -0,0 +1,18 @@
+var c = 1
+while (c <= 10) {
+    console.log(`Passo ${c}`)
+    c++
+}
+
+
+
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
---
+++ CursoJS/aula13/node_c357b3a080df7.tmp.js
@@ -0,0 +1,18 @@
+var c = 1
+while (c <= 100000000) {
+    console.log(`Passo ${c}`)
+    c++
+}
+
+
+
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
