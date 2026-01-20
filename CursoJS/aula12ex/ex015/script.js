ae8bf34bdc6c6ed8bcaf88e18a953cd3b99dba2f
ronaldoazevedo774-prog <ronaldoazevedo774@gmail.com>
2026-01-20 10:56:07 -0300

    finalizei o exercicio

--- CursoJS/aula12ex/ex015/script.js
+++ CursoJS/aula12ex/ex015/script.js
@@ -15,26 +15,35 @@ function verificar() {
             gênero = 'Homem'
             if (idade >=0 && idade < 10) {
                 //Criança
+                img.setAttribute('src', 'foto-bebe-m.jpg')
             } else if (idade < 21) {
                 //Jovem
+                img.setAttribute('src', 'foto-jovem-m.jpg')
             } else if (idade < 50) {
                 //Adulto
+                img.setAttribute('src', 'foto-adulto-m.jpg')
             } else {
                 //Idoso
+                img.setAttribute('src', 'foto-idoso-m.jpg')
             }
         } else if (fsex[1].checked) {
             gênero = 'Mulher'
             if (idade >=0 && idade < 10) {
                 //Criança
+                img.setAttribute('src', 'foto-bebe-f.jpg')
             } else if (idade < 21) {
                 //Jovem
+                img.setAttribute('src', 'foto-jovem-f.jpg')
             } else if (idade < 50) {
                 //Adulto
+                img.setAttribute('src', 'foto-adulto-f.jpg')
             } else {
                 //Idoso
+                img.setAttribute('src', 'foto-idoso-f.jpg')
             }
         }
         res.style.textAling = 'center'
         res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
