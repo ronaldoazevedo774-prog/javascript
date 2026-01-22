4166a8d635d78b392effeb668ee7fb10963f5600
ronaldoazevedo774-prog <ronaldoazevedo774@gmail.com>
2026-01-22 18:02:56 -0300

    atualizei o exercicio

---
+++ CursoJS/aula15/ambiente.js
@@ -0,0 +1,8 @@
+let num = [5, 8, 2, 9, 3]
+
+num.log(0)
+num.push(1)
+console.log(`Nosso vetor é o ${num.legth} posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+let pos = num.indexOf(8)
+console.log(`O valor 8 está na posição ${pos}`)
