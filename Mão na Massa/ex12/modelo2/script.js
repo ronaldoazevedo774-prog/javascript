69bc74aad98ada3da05de7cf9c4bb27e6d3404ea
ronaldoazevedo774-prog <ronaldoazevedo774@gmail.com>
2026-01-23 14:31:53 -0300

    terminei o projeto mais não descobri onde errei no JS

--- Mão na Massa/ex12/modelo2/script.js
+++ Mão na Massa/ex12/modelo2/script.js
@@ -1,9 +1,10 @@
+
 function verificar() {
     var data = new Date()
     var ano = data.getFullYear()
     var fano = document.getElementById('txtano')
     var res = document.querySelector('div#res')
-    if (fano.value.length == 0 || Number(fano.value) > ano) {
+    if (fano.value.legth == 0 || Number(fano.value)  > ano) {
         window.alert('[ERRO] Verifique os dados e tente novamente!')
     } else {
         var fsex = document.getElementsByName('radsex')
@@ -13,20 +14,21 @@ function verificar() {
         img.setAttribute('id', 'foto')
         if (fsex[0].checked) {
             gênero = 'Homem'
-            if (idade >=0 && idade < 10) {
-                //Criança
-                img.setAttribute('src', 'foto-bebe-m.jpg')
-            } else if (idade < 21) {
-                //Jovem
-                img.setAttribute('src', 'foto-jovem-m.jpg')
-            } else if (idade < 50) {
-                //Adulto
-                img.setAttribute('src', 'foto-adulto-m.jpg')
-            } else {
-                //Idoso
-                img.setAttribute('src', 'foto-idoso-m.jpg')
-            }
-        } else if (fsex[1].checked) {
+        
+        if (idade >= 0 && idade < 10) {
+            //CRIANÇA
+            img.setAttribute('src', 'foto-bebe-m.jpg')
+        } else if (idade < 21) {
+            //JOVEM
+            img.setAttribute('src', 'foto-jovem-m.jpg')
+        } else if (idade > 50) {
+            //ADULTO
+            img.setAttribute('src', 'foto-adulto-m.jpg')
+        } else {
+            //IDOSO
+            img.setAttribute('src', 'foto-idoso-m.jpg')
+        } 
+    } else if (fsex[1].checked) {
             gênero = 'Mulher'
             if (idade >=0 && idade < 10) {
                 //Criança
@@ -43,7 +45,8 @@ function verificar() {
             }
         }
         res.style.textAling = 'center'
-        res.innerHTML = `Detectamos ${gênero} com ${ano} anos.`
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
         res.appendChild(img)
-    }
+    } 
+       
 }
